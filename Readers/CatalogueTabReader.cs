316182750ca0ceccfe3d9cca007cce429b6b8c64
Alex Miller <TheAmazingAussie@users.noreply.github.com>
2017-10-10 07:09:32 +1000

    Save with better formatting for parsing tabs

--- Readers/CatalogueTabReader.cs
+++ Readers/CatalogueTabReader.cs
@@ -12,6 +12,8 @@ namespace Katalogi.Readers
     public class CatalogueTabReader
     {
         private static Random random = new Random();
+        private static int increment = -1;
+
         private Form1 form;
 
         public CatalogueTabReader(Form1 form)
@@ -33,11 +35,13 @@ namespace Katalogi.Readers
             tabData.tabs = new List<dynamic>();
 
             tabData.isEnabled = packet.ReadBoolean();
-
             tabData.iconImage = packet.ReadInteger();
 
+            tabData.pageId = packet.ReadInteger();
+
             tabData.parentId = -1;
-            tabData.id = packet.ReadInteger();
+            tabData.id = increment;
+            increment++;
 
             tabData.pageLink = packet.ReadString();
             tabData.pageCaption = packet.ReadString();
@@ -51,11 +55,53 @@ namespace Katalogi.Readers
             int subPages = packet.ReadInteger();
             for (int i = 0; i < subPages; i++)
             {
-                tabData.tabs.Add(this.ReadTabData(obj));
+                dynamic tab = this.ReadTabData(obj);
+                tab.parentId = tabData.id;
+
+                tabData.tabs.Add(tab);
             }
 
             return tabData;
         }
+
+        /*        private dynamic ReadTabData(DataInterceptedEventArgs obj)
+        {
+            var packet = obj.Packet;
+            dynamic tabData = new ExpandoObject();
+       
+            tabData.tabs = new List<dynamic>();
+     
+            tabData.isEnabled = packet.ReadBoolean();
+            tabData.iconImage = packet.ReadInteger();
+
+            tabData.parentId = -1;
+            tabData.id = packet.ReadInteger();
+
+            if (tabData.id == -1)
+            {
+                tabData = incrementId;
+                incrementId++;
+            }
+
+            tabData.pageLink = packet.ReadString();
+            tabData.pageCaption = packet.ReadString();
+
+            int items = packet.ReadInteger();
+            for (int i = 0; i < items; i++)
+            {
+                packet.ReadInteger();
+            }
+
+            int subPages = packet.ReadInteger();
+            for (int i = 0; i < subPages; i++)
+            {
+                dynamic newTab = this.ReadTabData(obj);
+                newTab.parentId = tabData.id;
+                tabData.tabs.Add(newTab);
+            }
+
+            return tabData;
+        }*/
 
         /*private void ReadChildTabs(dynamic parentTab, HMessage packet)
         {
