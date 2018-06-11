0b7310a3e1fb414ac8a9f2d2076a8684efdd7b9b
Speaqer <speaqerpr0@gmail.com>
2018-06-11 23:32:32 +0300

    Cleaned and updated for latest Tanji (v1.4.1110)

---
+++ Habbo/HCatalogItem.cs
@@ -0,0 +1,75 @@
+﻿using Sulakore.Protocol;
+
+namespace Katalogi.Habbo
+{
+    public class HCatalogItem
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+
+        public int CostCredits { get; set; }
+
+        public int OtherCurrencyCost { get; set; }
+        public int OtherCurrencyType { get; set; }
+
+        public bool AllowGift { get; set; }
+
+        public bool HasBadge { get; set; }
+        public string BadgeType { get; set; }
+        public string BadgeCode { get; set; }
+
+        public string Type { get; set; }
+        public string Name { get; set; }
+
+        public int SpriteId { get; set; }
+        public string ExtraData { get; set; }
+
+        public int Amount { get; set; }
+
+        public bool IsLimited { get; set; }
+
+        public int LimitedTotal { get; set; }
+        public int LimitedRemaining { get; set; }
+
+        public int SubscriptionStatus { get; set; }
+
+        public bool AllowMultiple { get; set; }
+
+        public HCatalogItem(HMessage packet)
+        {
+            Id = packet.ReadInteger();
+            DisplayName = packet.ReadString();
+            packet.ReadBoolean();
+            CostCredits = packet.ReadInteger();
+            OtherCurrencyCost = packet.ReadInteger();
+            OtherCurrencyType = packet.ReadInteger();
+            AllowGift = packet.ReadBoolean();
+            if(HasBadge = (packet.ReadInteger() == 2))
+            {
+                BadgeType = packet.ReadString();
+                BadgeCode = packet.ReadString();
+            }
+
+            Type = packet.ReadString();
+
+            if (HasBadge) Name = packet.ReadString();
+            else
+            {
+                SpriteId = packet.ReadInteger();
+                ExtraData = packet.ReadString();
+                Amount = packet.ReadInteger();
+                if(IsLimited = packet.ReadBoolean())
+                {
+                    LimitedTotal = packet.ReadInteger();
+                    LimitedRemaining = packet.ReadInteger();
+                }
+            }
+
+            SubscriptionStatus = packet.ReadInteger();
+            AllowMultiple = packet.ReadBoolean();
+
+            packet.ReadBoolean();
+            packet.ReadString();
+        }
+    }
+}
---
+++ Habbo/HCatalogTab.cs
@@ -0,0 +1,47 @@
+﻿using Sulakore.Protocol;
+using System.Collections.Generic;
+
+namespace Katalogi.Habbo
+{
+    public class HCatalogTab
+    {
+        public bool IsEnabled { get; set; }
+        public int IconImage { get; set; }
+        public int PageId { get; set; }
+        public int Id { get; set; }
+
+        public int ParentId { get; set; }
+
+        public string Link { get; set; }
+        public string Caption { get; set; }
+
+        public List<int> Items { get; set; }
+
+        public List<HCatalogTab> SubPages { get; set; }
+
+        public HCatalogTab(HMessage packet, int parentId = -1, int incrementId = -1)
+        {
+            IsEnabled = packet.ReadBoolean();
+            IconImage = packet.ReadInteger();
+            PageId = packet.ReadInteger();
+
+            ParentId = parentId;
+            Id = incrementId++;
+
+            Link = packet.ReadString();
+            Caption = packet.ReadString();
+
+            Items = new List<int>(packet.ReadInteger());
+            for(int i = 0; i < Items.Capacity; i++)
+            {
+                Items.Add(packet.ReadInteger());
+            }
+
+            SubPages = new List<HCatalogTab>(packet.ReadInteger());
+            for (int i = 0; i < SubPages.Capacity; i++)
+            {
+                SubPages.Add(new HCatalogTab(packet, Id, incrementId));
+            }
+        }
+    }
+}
